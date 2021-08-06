f0cdb4fdbd2b5234043df330cfcb73b984ab64d6
HI-HARINI <77035916+HI-HARINI@users.noreply.github.com>
2021-08-05 19:49:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+Webcam.set({
+    width:350,
+    height:350,
+    png_quality:90
+})
+camera=document.getElementById("camera")
+Webcam.attach('#camera')
+function takesnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("pic").innerHTML='<img id="image" src="'+data_uri+'" width="350" height="300">'
+    
+    })
+}
+console.log(ml5.version)
+classifier=ml5.imageClassifier('MobileNet',modelLoaded)
+function modelLoaded(){
+    console.log("modelLoadel")
+}
+function check(){
+    img=document.getElementById('image');
+    classifier.classify(img,gotResult)
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("objectname").innerHTML=results[0]
+    }
+}
